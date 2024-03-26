c00a91e4071c0c507c8bb59389a8b8db3ee6694c
Julia Xu <juliaxu234@gmail.com>
2024-03-26 17:05:00 -0400

    added function to check if a node is within another node to node.py, and added the add_node function to database.py

--- bin_database/database.py
+++ bin_database/database.py
@@ -1,2 +1,4 @@
 class Database:
-    pass    
+    def __init__(self,min_lat,max_lat,min_long,max_long):
+        self.head_node = Node(min_lat,max_lat,min_long,max_long)
+        
--- bin_database/node.py
+++ bin_database/node.py
@@ -1,12 +1,12 @@
 class Node:
-    def __init__(self):
+    def __init__(self, min_lat, max_lat, min_long, max_long):
         self.parent=Node()
-        self.children=[Node()]
+        self.children=[None, None]
 #the values below will all be floats
-        self.min_lat = 0.0
-        self.max_lat = 0.0
-        self.min_long = 0.0
-        self.max_long = 0.0
+        self.min_lat = min_lat
+        self.max_lat = max_lat
+        self.min_long = min_long
+        self.max_long = max_long
         self.bin = bin()
 
     def get_min_lat(self):
@@ -28,3 +28,15 @@ class Node:
     def update_long(self, min_long_new, max_long_new):
         self.min_long= min_long_new
         self.max_ling=max_long_new
+    def within_lat(self, node1): #checks if node1 is within the current node (for the latitude bounds)
+        if self.min_lat < node1.min_lat:
+            if self.max_lat > node1.max_lat:
+                return True
+        else:
+            return False
+    def within_long(self,node1): #checks if node1 is within the current node (for longitude bounds)
+        if self.min_long < node1.min_long:
+            if self.max_long > node1.max_long:
+                return True
+        else:
+            return False
