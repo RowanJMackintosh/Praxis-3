b2979c2df559c5048c76a34a60f0f103477ddc99
reimyousef <reim.yousef@gmail.com>
2024-03-26 13:17:44 -0400

    addedd main functions of node.py class

--- bin_database/node.py
+++ bin_database/node.py
@@ -1,2 +1,17 @@
 class Node:
-    pass
+    def __init__(self):
+    
+        self.parent=Node()
+        self.children=[Node()]
+#the values below will all be floats
+        self.min_lat
+        self.max_lat
+        self.min_long
+        self.max_long
+        self.bin = Bin()
+
+    def get_min_lat():
+        return self.min_lat
+    
+    def update_min_lat():
+        
