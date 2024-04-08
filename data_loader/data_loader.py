ea4cc8943cdf263c58e3778d88d5efb60c1eeb69
Aniruddh <yergol2020@gmail.com>
2024-04-08 16:45:55 -0400

    rewrote data_loader as method

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -2,24 +2,17 @@ import time
 from Adafruit_IO import Client
 import os
 
-prev_string = None
-clear_file = 0
-open("data_loader/readings.txt", 'w').close()
 
-while True:
-    time.sleep(10)
-    
+def load_data_to_file():
+    open("data_loader/readings.txt", 'w').close()
     client = Client("aragolaa", "aio_LfUu68p7cpa5GuH3rscznuLAFkVj")
-    
+
     data_string = client.receive("information") 
-    
-    if prev_string != data_string:
-        if os.path.exists("data_loader/readings.txt"):
-            clear_file = (clear_file + 1)%30
-            with open("data_loader/readings.txt", "a+") as file:
-                file.write(data_string.value + " | " + data_string.updated_at + "\n")
-    
-    if clear_file == 29:
-        open("data_loader/readings.txt", 'w').close()
+    if os.path.exists("data_loader/readings.txt"):
+        clear_file = (clear_file + 1)%30
+        with open("data_loader/readings.txt", "a+") as file:
+            file.write(data_string.value + " | " + data_string.updated_at + "\n")
+            
+        time.sleep(10)
         
         
